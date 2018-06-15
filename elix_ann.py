32a2cb56d14af2aa256b532e0cfa22c536a63445
Elixander <elixias@gmail.com>
2018-06-15 18:40:51 +0800

    Added KFold Validation, GridSearch for Parameter Tuning

--- elix_ann.py
+++ elix_ann.py
@@ -31,21 +31,64 @@ X_test = sc.transform(X_test)
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
-
-classifier = Sequential()
-classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu',input_dim=11))
-classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu'))
-classifier.add(Dense(units=1,kernel_initializer='uniform',activation='sigmoid'))
-
-#loss='categorical_crossentropy' for more than 2 cats. both are logarithmic losses.
-classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-classifier.fit(X_train,y_train,batch_size=10,epochs=10,verbose=1)
-
+from keras.layers import Dropout
+"""
+#classifier.fit(X_train,y_train,batch_size=10,epochs=10,verbose=1)
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred>0.5) #!!!
-
 #print results as confusion matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
 print(cm)
+#predicting result for new customer
+customer = sc.transform(np.array([[0,0,600,1,40,3,60000,2,1,1,50000]]))
+print(customer)
+print((classifier.predict(customer)>0.5))
+"""
+
+#import keras classifier for kfold 
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+
+def build_classifier():
+    classifier = Sequential()
+    #layer 1 with dropout
+    classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu',input_dim=11))
+    classifier.add(Dropout(p=0.1))#disable 10% of neurons
+    classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu'))
+    classifier.add(Dense(units=1,kernel_initializer='uniform',activation='sigmoid'))
+    #loss='categorical_crossentropy' for more than 2 cats. both are logarithmic losses.
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn=build_classifier,batch_size=10,epochs=1)
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train,cv=10,n_jobs=-1) #accs returned by kfold
+print("Accuracies",accuracies)
+print("Mean",accuracies.mean())
+print("Variance",accuracies.std())
+
+#performing gridsearch for hyperparameter tuning
+from sklearn.model_selection import GridSearchCV
+def build_classifier(optimizer):
+    classifier = Sequential()
+    #layer 1 with dropout
+    classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu',input_dim=11))
+    classifier.add(Dropout(p=0.1))#disable 10% of neurons
+    classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu'))
+    classifier.add(Dense(units=1,kernel_initializer='uniform',activation='sigmoid'))
+    #loss='categorical_crossentropy' for more than 2 cats. both are logarithmic losses.
+    classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn=build_classifier)
+parameters = {
+    'batch_size':[25,32],
+    'epochs':[10,50],
+    'optimizer':['adam','rmsprop']
+}
+gridsearch = GridSearchCV(estimator=classifier,
+                         param_grid=parameters,
+                         scoring='accuracy',
+                         cv=10)
+gridsearch = gridsearch.fit(X_train,y_train)
+best_params = gridsearch.best_params_
+best_accuracy = gridsearch.best_score_
+print(best_params,best_accuracy)
