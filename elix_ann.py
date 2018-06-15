779681a94b37c42fe43a6deb2ffebf6c3f48286a
Elixander <elixias@gmail.com>
2018-06-15 14:24:21 +0800

    Running version with 10 epochs

--- elix_ann.py
+++ elix_ann.py
@@ -4,23 +4,25 @@ import pandas as pd
 
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:,3:-1].values
-Y = dataset.iloc[:,-1].values
+y = dataset.iloc[:,-1].values
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
-X[:,1] = labelencoder_X.fit_transform(X[:,1])
+X[:,1] = labelencoder_X.fit_transform(X[:,1]) #geography
+labelencoder_G = LabelEncoder()
+X[:,2] = labelencoder_G.fit_transform(X[:,2])
+
 onehotencoder = OneHotEncoder(categorical_features=[1]) #dummy variables
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:,1:]
-labelencoder_X = LabelEncoder()
-X[:,2] = labelencoder_X.fit_transform(X[:,2])
+
 
 #change cross_validation to model_selection
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0)
 
-from sklearn.preprocessing import StandardScalar
-sc = StandardScalar()
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
@@ -36,10 +38,11 @@ classifier.add(Dense(output_dim=1,init='uniform',activation='sigmoid'))
 #loss='categorical_crossentropy' for more than 2 cats. both are logarithmic losses.
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-classifier.fit(X_train,Y_train,batch_size=10,nb_epoch=100,verbose=1)
+classifier.fit(X_train,y_train,batch_size=10,nb_epoch=10,verbose=1)
 
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred>0.5) #!!!
 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
+print(cm)
